3308edde216b0b322b5ebe98c13b2739bd00d33c
Yuya Harada <tzou11291996@gmail.com>
2020-04-06 23:58:06 +0900

    took code from Garrett

--- src/loscore.js
+++ src/loscore.js
@@ -8,8 +8,22 @@ class LoScore {
   | ARRAYS
   |~~~~~~~~~~
   * */
+
   uniq(array) {
-    // YOUR CODE HERE
+    let result = [];
+    let test = false;
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < result.length; j++) {
+        if (array[i] === result[j]) {
+          test = true;
+        }
+      }
+      if (test === false) {
+        result.push(array[i]);
+      }
+      test = false;
+    }
+    return result;
   }
 
   /**
@@ -30,7 +44,9 @@ class LoScore {
   }
 
   map(collection, iteratee) {
-    // YOUR CODE HERE
+    let result = [];
+    this.each(collection, (val) => result.push(iteratee(val)));
+    return result;
   }
 
   filter(collection, test) {
@@ -39,22 +55,54 @@ class LoScore {
     return result;
   }
 
-  reject(collection, test) {}
-
-  reduce(collection, iterator, accumulator) {
-    // YOUR CODE HERE
+  reject(collection, test) {
+    let result = [];
+    this.filter(collection, (val) => {
+      if (!test(val)) {
+        result.push(val);
+      }
+    });
+    return result;
   }
 
-  every() {
-    // YOUR CODE HERE
+  reduce(collection, iterator, accumulator) {
+    let result = accumulator;
+    if (typeof accumulator === "undefined") {
+      result = collection[0];
+      collection = collection.slice(1);
+    }
+    this.each(collection, (val) => {
+      result = iterator(result, val);
+    });
+    return result;
+  }
+
+  every(collection, iterator) {
+    if (typeof iterator === "undefined") {
+      return true;
+    }
+    return this.reduce(
+      collection,
+      (acc, val) => {
+        if (!acc) {
+          return false;
+        } else return iterator(val);
+      },
+      true
+    );
   }
 
   /**
   | OBJECTS
   |~~~~~~~~~~
   * */
-  extend(obj) {
-    // YOUR CODE HERE
+  extend(objA, ...objB) {
+    for (let item of objB) {
+      this.each(item, (val, key) => {
+        objA[key] = val;
+      });
+    }
+    return objA;
   }
 
   /**
@@ -63,18 +111,45 @@ class LoScore {
   * */
 
   once(func) {
-    // YOUR CODE HERE
+    let wasCalled = false;
+    let result = 0;
+    return function() {
+      if (wasCalled === false) {
+        result = func();
+        wasCalled = true;
+      }
+      return result;
+    };
   }
 
   memoize(func) {
-    // YOUR CODE HERE
+    let cache = {};
+    return function(...args) {
+      if (cache[args]) {
+        return cache[args];
+      } else {
+        let val = func(args);
+        cache[args] = val;
+        return val;
+      }
+    };
   }
 
   invoke(collection, functionOrKey) {
-    // YOUR CODE HERE
+    let result = [];
+    if (typeof functionOrKey !== "function") {
+      this.each(collection, (val, index) => {
+        result.push(collection[index][functionOrKey].apply(val));
+      });
+    } else {
+      this.each(collection, (val) => {
+        result.push(functionOrKey.apply(val));
+      });
+    }
+    return result;
   }
 
-  /**
+  /**  
   | ADVANCED REQUIREMENTS
   |~~~~~~~~~~~~~
   * */
